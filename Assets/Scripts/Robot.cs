ff9eae17acfdfd5dbc56a477eb5bba4ca6636453
Carlos Emiliano Castro Trejo <carlos_castro5156@outlook.es>
2018-06-15 22:12:10 -0500

    Camera is set, fighter prefab is added correctly, animations are missing for inputs

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Robot : MonoBehaviour {
+
+	public Animator animation;
+
+	public FixedJoystick MoveJoystick;
+	public FixedButton JumpButton;
+	public FixedTouchField TouchField;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animation = GetComponent<Animator> ();
+		
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		var fps = GetComponent<RigidbodyFirstPersonController> ();
+
+		fps.RunAxis = MoveJoystick.inputVector; 
+		fps.JumpAxis = JumpButton.Pressed; 
+		fps.mouseLook.LookAxis = TouchField.TouchDist;
+
+
+
+	}
+}
